b41259809ccc2586e60f81209bd6198b131e1cab
tutkowskim <tutkows1@gmail.com>
2021-01-17 23:42:21 -0600

    Create a routes file for azure

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './login/login.component';
 import { AuthGuardGuard } from './auth/auth-guard.guard';
 import { DisabledUserComponent } from './disabled-user/disabled-user.component';
 
+// When adding routes be sure to update routes.json, so that azure will allow direct navigation to it.
 const routes: Routes = [
   { path: '', component: TasksComponent, canActivate: [AuthGuardGuard] },
   { path: 'disabled-user', component: DisabledUserComponent },
