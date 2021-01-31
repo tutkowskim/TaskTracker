f606a71861699655861e6a6af09cb947dbbc6a95
Mark Tutkowski <mark@tutkowski.com>
2021-01-31 16:06:28 -0600

    Fetch task data from the backend

--- frontend-react/src/Tasks/Tasks.js
+++ frontend-react/src/Tasks/Tasks.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import IconButton from '@material-ui/core/IconButton';
 import TextField from '@material-ui/core/TextField';
 import List from '@material-ui/core/List';
@@ -12,10 +13,23 @@ import CheckBoxIcon from '@material-ui/icons/CheckBox';
 import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
 import DeleteIcon from '@material-ui/icons/Delete';
 import { ListItemSecondaryAction } from '@material-ui/core';
+import useInterval from '../useInterval';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
+  },
+  verticalCenterContents: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  horizontalCenterContents: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
   addTaskRow: {
     display: 'flex',
@@ -26,9 +40,41 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const useTasks = () => {
+  const [areTasksLoading, setAreTasksLoading] = useState(true);
+  const [tasks, setTasks] = useState([{ name: 'Initial Task', complete: true }]);
+
+  const fetchTasks = async () => {
+    const response = await fetch('/api/GetTasks');
+    const data = await response.json();
+    setTasks(data);
+    setAreTasksLoading(false);
+  }
+
+  // Fetch initial data
+  useEffect(() => fetchTasks(), []);
+
+  // Refresh the data
+  useInterval(() => fetchTasks(), 5000);
+
+  const setTasksWrapper = (updatedTasks) => {
+    //Update the state
+    setTasks(updatedTasks);
+
+    // Post the new tasks back, so that they are saved
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(updatedTasks)
+    };
+    fetch('/api/SetTasks', requestOptions);
+  }
+  return [areTasksLoading, tasks, setTasksWrapper];
+}
+
 function Tasks() {
   const classes = useStyles();
-  const [tasks, setTasks] = useState([{ name: 'Initial Task', complete: true }]);
+  const [areTasksLoading, tasks, setTasks] = useTasks();
   const [newTaskName, setNewTaskName] = useState('');
 
   const onAddTask = () => {
@@ -45,6 +91,16 @@ function Tasks() {
     setTasks([...tasks]);
   };
 
+  if (areTasksLoading) {
+    return (
+      <div className={classes.verticalCenterContents}>
+        <div className={classes.horizontalCenterContents}>
+          <CircularProgress size="100px" />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className={classes.root}>
       <div className={classes.addTaskRow}>
@@ -58,12 +114,12 @@ function Tasks() {
             <ListItemIcon>
               { task.complete ? <CheckBoxIcon /> : <CheckBoxOutlineBlankIcon /> }
             </ListItemIcon>
+            <ListItemText primary={task.name}/>
             <ListItemSecondaryAction onClick={(event) => {onRemoveTask(task); event.stopPropagation()}}>
               <IconButton edge="end" aria-label="delete">
                 <DeleteIcon />
               </IconButton>
             </ListItemSecondaryAction>
-            <ListItemText primary={task.name}/>
           </ListItem>
         )}
       </List>
